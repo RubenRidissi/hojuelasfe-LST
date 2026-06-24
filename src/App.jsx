45f9b0a16fc0e3440310e6986617b22c7db0ec82
RubenRidissi <rridissi@gmail.com>
2026-06-24 19:43:07 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import PedidosPage from './pages/PedidosPage'
 import VentasPage from './pages/VentasPage'
 import PagosPage from './pages/PagosPage'
 import RemitosPage from './pages/RemitosPage'
+import CtaCorrientePage from './pages/CtaCorrientePage'
 import Sidebar from './components/Sidebar'
 import MobileNav from './components/MobileNav'
 import FabButton from './components/FabButton'
@@ -29,6 +30,7 @@ function AppLayout() {
           <Route path="/ventas"   element={<VentasPage />} />
           <Route path="/pagos"    element={<PagosPage />} />
           <Route path="/remitos"  element={<RemitosPage />} />
+          <Route path="/ctacte"   element={<CtaCorrientePage />} />
           <Route path="*"         element={<Navigate to="/" replace />} />
         </Routes>
       </main>
