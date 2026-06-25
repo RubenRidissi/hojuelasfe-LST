e37948ba9321812d85af3db5a89098ef490b0f42
RubenRidissi <rridissi@gmail.com>
2026-06-24 23:25:08 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { initFontSize } from './pages/ConfigPage'
+initFontSize()
 import App from './App'
 import { AuthProvider } from './context/AuthContext'
 
